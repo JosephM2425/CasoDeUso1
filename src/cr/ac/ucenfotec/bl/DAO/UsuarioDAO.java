5ce7d3a76e4e43c3f3c973d921441d6939b16e32
Carolina Arias <128652671+cariasj03@users.noreply.github.com>
2023-06-08 22:13:33 -0600

    Added UsuarioDAO and other comments

---
+++ src/cr/ac/ucenfotec/bl/DAO/UsuarioDAO.java
@@ -0,0 +1,189 @@
+package cr.ac.ucenfotec.bl.DAO;
+import cr.ac.ucenfotec.bl.entities.Usuario;
+import cr.ac.ucenfotec.bl.config.Configuracion;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * @author Carolina Arias
+ * @version 1.0
+ * @since 08/06/2023
+ *
+ * Esta clase se encarga de gestionar el acceso a datos de los objetos Usuario
+ */
+public class UsuarioDAO {
+    /**
+     * Metodo para registrar un usuario
+     * @param usuario es de tipo Usuario y corresponde al usuario por registrar
+     * @return 0 si se registro correctamente, 1 si no se pudo registrar
+     */
+    public int registrarUsuario(Usuario usuario) {
+        try {
+            //Se crea una nueva instancia del archivo de configuración
+            Configuracion configuracion= new Configuracion();
+            //Lo lee del archivo de configuracion
+            Class.forName(configuracion.getClaseJDBC());
+            Connection conn = null;
+            PreparedStatement stmt = null;
+
+            String strConexion = configuracion.getStringConexion();
+            String query = "EXECUTE sp_registrar_usuario ?, ?, ?, ?, ?, ?";
+            conn = DriverManager.getConnection(strConexion);
+            stmt = conn.prepareStatement(query);;
+            stmt.setString(1,usuario.getNombre_completo());
+            stmt.setString(2,usuario.getDireccion());
+            stmt.setString(3,usuario.getTelefono());
+            stmt.setString(4,usuario.getNombre_usuario());
+            stmt.setString(5,usuario.getContrasena());
+            stmt.setString(6,usuario.getRol());
+            stmt.execute();
+
+            return 0;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+    /**
+     * Metodo para listar los usuarios
+     * @return un ArrayList con los usuarios
+     */
+    public ArrayList<Usuario> listarUsuarios()
+    {
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+        try {
+            Configuracion configuracion= new Configuracion();
+            Class.forName(configuracion.getClaseJDBC());
+            Connection conn = null;
+            String query = "SELECT * FROM vw_usuarios";
+            Statement stmt = null;
+            ResultSet rs = null;
+            String strConexion = configuracion.getStringConexion();
+            conn = DriverManager.getConnection(strConexion);
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("ID_USUARIO"));
+                usuario.setNombre_completo(rs.getString("NOMBRE_COMPLETO"));
+                usuario.setDireccion(rs.getString("DIRECCION"));
+                usuario.setTelefono(rs.getString("TELEFONO"));
+                usuario.setNombre_usuario(rs.getString("NOMBRE_USUARIO"));
+                usuario.setContrasena(rs.getString("CONTRASENA"));
+                usuario.setRol(rs.getString("ROL"));
+                usuarios.add(usuario);
+            }
+            conn.close();
+        } catch (Exception e){
+            return null;
+        }
+        return usuarios;
+    }
+
+    /**
+     * Metodo para modificar un usuario
+     * @param usuario es de tipo Usuario y corresponde al usuario por modificar
+     * @return 0 si se modifico correctamente, 1 si no se pudo modificar
+     */
+    public int modificarUsuario(Usuario usuario) {
+        try {
+            //Se crea una nueva instancia del archivo de configuración
+            Configuracion configuracion= new Configuracion();
+            //Lo lee del archivo de configuracion
+            Class.forName(configuracion.getClaseJDBC());
+            Connection conn = null;
+            PreparedStatement stmt = null;
+            String strConexion = configuracion.getStringConexion();
+            String query = "EXECUTE sp_modificar_usuario ?, ?, ?, ?, ?, ?, ?";
+            conn = DriverManager.getConnection(strConexion);
+            stmt = conn.prepareStatement(query);;
+            stmt.setInt(1,usuario.getId());
+            stmt.setString(2,usuario.getNombre_completo());
+            stmt.setString(3,usuario.getDireccion());
+            stmt.setString(4,usuario.getTelefono());
+            stmt.setString(5,usuario.getNombre_usuario());
+            stmt.setString(6,usuario.getContrasena());
+            stmt.setString(7,usuario.getRol());
+            stmt.execute();
+            return 0;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+    /**
+     * Metodo para buscar un usuario por su nombre de usuario
+     * @param nombreUsuario es de tipo String y corresponde al nombre de usuario del usuario por buscar
+     * @return usuario es de tipo Usuario y corresponde al usuario por buscar
+     */
+    public Usuario buscarUsuarioPorNombreUsuario(String nombreUsuario)
+    {
+        Configuracion configuracion = new Configuracion();
+        Usuario usuario = new Usuario();
+        try {
+            Class.forName(configuracion.getClaseJDBC());
+            String strConexion = configuracion.getStringConexion();
+            Connection conn = null;
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            conn = DriverManager.getConnection(strConexion);
+            String query = "EXECUTE sp_buscar_usuario_por_nombre_de_usuario ?";
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1, nombreUsuario);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                usuario.setId(rs.getInt("ID_USUARIO"));
+                usuario.setNombre_completo(rs.getString("NOMBRE_COMPLETO"));
+                usuario.setDireccion(rs.getString("DIRECCION"));
+                usuario.setTelefono(rs.getString("TELEFONO"));
+                usuario.setNombre_usuario(rs.getString("NOMBRE_USUARIO"));
+                usuario.setContrasena(rs.getString("CONTRASENA"));
+                usuario.setRol(rs.getString("ROL"));
+            }
+            conn.close();
+        } catch (Exception e){
+            return null;
+        }
+        return usuario;
+    }
+
+    /**
+     * Metodo para buscar un usuario por su id
+     * @param idUsuario es de tipo int y corresponde al id del usuario por buscar
+     * @return usuario es de tipo Usuario y corresponde al usuario por buscar
+     */
+    public Usuario buscarUsuarioPorId(int idUsuario)
+    {
+        Configuracion configuracion = new Configuracion();
+        Usuario usuario = new Usuario();
+        try {
+            Class.forName(configuracion.getClaseJDBC());
+            String strConexion = configuracion.getStringConexion();
+            Connection conn = null;
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            conn = DriverManager.getConnection(strConexion);
+            String query = "EXECUTE sp_buscar_usuario_por_id ?";
+            stmt = conn.prepareStatement(query);
+            stmt.setInt(1, idUsuario);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                usuario.setId(rs.getInt("ID_USUARIO"));
+                usuario.setNombre_completo(rs.getString("NOMBRE_COMPLETO"));
+                usuario.setDireccion(rs.getString("DIRECCION"));
+                usuario.setTelefono(rs.getString("TELEFONO"));
+                usuario.setNombre_usuario(rs.getString("NOMBRE_USUARIO"));
+                usuario.setContrasena(rs.getString("CONTRASENA"));
+                usuario.setRol(rs.getString("ROL"));
+            }
+            conn.close();
+        } catch (Exception e){
+            return null;
+        }
+        return usuario;
+    }
+}
--- src/cr/ac/ucenfotec/bl/entities/Prestamo.java
+++ src/cr/ac/ucenfotec/bl/entities/Prestamo.java
@@ -1,16 +1,53 @@
 package cr.ac.ucenfotec.bl.entities;
 
-import java.util.Date;
+import java.time.LocalDate;
 
+/**
+ * @author Joseph Murillo
+ * @version 1.0
+ * @since 08/06/2023
+ *
+ * Esta clase se encarga de gestionar todos los objetos Prestamo
+ */
 public class Prestamo {
+    /**
+     * Declaracion de atributos del objeto
+     */
     private int id;
     private Libro libro;
     private Usuario usuario;
-    private Date fecha_prestamo;
-    private Date fecha_vencimiento;
-    private Date fecha_devolucion;
+    private LocalDate fecha_prestamo;
+    private LocalDate fecha_vencimiento;
+    private LocalDate fecha_devolucion;
 
-    public Prestamo(int id, Libro libro, Usuario usuario, Date fecha_prestamo, Date fecha_vencimiento, Date fecha_devolucion) {
+    //Seteo de los constructores
+
+    /**
+     * Este es el constructor por defecto
+     */
+    public Prestamo() {
+
+    }
+
+    public Prestamo(int id, Libro libro, Usuario usuario, LocalDate fecha_prestamo, LocalDate fecha_vencimiento) {
+        this.id = id;
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fecha_prestamo = fecha_prestamo;
+        this.fecha_vencimiento = fecha_vencimiento;
+    }
+
+    /**
+     * Crea una nueva instancia de la clase Prestamo
+     *
+     * @param id es de tipo int y corresponde al ID del préstamo
+     * @param libro es de tipo Libro y corresponde al libro prestado en el préstamo
+     * @param usuario es de tipo Usuario y corresponde al usuario que solicita el préstamo
+     * @param fecha_prestamo es de tipo LocalDate y corresponde a la fecha en que se realiza el préstamo
+     * @param fecha_vencimiento es de tipo LocalDate y corresponde a la fecha límite para la devolución del libro
+     * @param fecha_devolucion es de tipo LocalDate y corresponde a la fecha en que se devuelve el libro
+     */
+    public Prestamo(int id, Libro libro, Usuario usuario, LocalDate fecha_prestamo, LocalDate fecha_vencimiento, LocalDate fecha_devolucion) {
         this.id = id;
         this.libro = libro;
         this.usuario = usuario;
@@ -19,6 +56,9 @@ public class Prestamo {
         this.fecha_devolucion = fecha_devolucion;
     }
 
+    /**
+     * Getters y setters de los atributos del objeto
+     */
     public int getId() {
         return id;
     }
@@ -43,27 +83,43 @@ public class Prestamo {
         this.usuario = usuario;
     }
 
-    public Date getFecha_prestamo() {
+    public LocalDate getFecha_prestamo() {
         return fecha_prestamo;
     }
 
-    public void setFecha_prestamo(Date fecha_prestamo) {
+    public void setFecha_prestamo(LocalDate fecha_prestamo) {
         this.fecha_prestamo = fecha_prestamo;
     }
 
-    public Date getFecha_vencimiento() {
+    public LocalDate getFecha_vencimiento() {
         return fecha_vencimiento;
     }
 
-    public void setFecha_vencimiento(Date fecha_vencimiento) {
+    public void setFecha_vencimiento(LocalDate fecha_vencimiento) {
         this.fecha_vencimiento = fecha_vencimiento;
     }
 
-    public Date getFecha_devolucion() {
+    public LocalDate getFecha_devolucion() {
         return fecha_devolucion;
     }
 
-    public void setFecha_devolucion(Date fecha_devolucion) {
+    public void setFecha_devolucion(LocalDate fecha_devolucion) {
         this.fecha_devolucion = fecha_devolucion;
+    }
+
+    /**
+     * Metodo que devuelve los atributos en formato String
+     * @return devuelve todos los atributos del objeto en formato String
+     */
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "id=" + id +
+                ", libro=" + libro +
+                ", usuario=" + usuario +
+                ", fecha_prestamo=" + fecha_prestamo +
+                ", fecha_vencimiento=" + fecha_vencimiento +
+                ", fecha_devolucion=" + fecha_devolucion +
+                '}';
     }
 }
