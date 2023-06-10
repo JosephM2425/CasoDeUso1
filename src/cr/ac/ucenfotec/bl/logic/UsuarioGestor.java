547dd3dbb1974b55c09fe557b020c6135bded919
usoza0497 <asozas@ucenfotec.ac.cr>
2023-06-09 20:50:10 -0600

    Implementacion de metodos en el gestor usuario

--- src/cr/ac/ucenfotec/bl/logic/UsuarioGestor.java
+++ src/cr/ac/ucenfotec/bl/logic/UsuarioGestor.java
@@ -1,4 +1,61 @@
 package cr.ac.ucenfotec.bl.logic;
 
+import cr.ac.ucenfotec.bl.DAO.UsuarioDAO;
+import cr.ac.ucenfotec.bl.entities.Usuario;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class UsuarioGestor {
+    private UsuarioDAO usuarioDAO;
+
+    public UsuarioGestor() {
+        this.usuarioDAO = new UsuarioDAO();
+    }
+
+    public String registrarUsuario(Usuario usuario) {
+        if (!existeUsuario(usuario)) {
+            int resultadoRegistro = usuarioDAO.registrarUsuario(usuario);
+            if (resultadoRegistro == 0) {
+                return "Registro Exitoso!";
+            } else {
+                return "Hubo un error en el registro, porfavor verifique los datos";
+            }
+        } else {
+            return "Usuario ya existente";
+        }
+    }
+
+    public ArrayList<Usuario> listarUsuarios() {
+        return usuarioDAO.listarUsuarios();
+    }
+
+    public String modificarUsuario(Usuario usuario) {
+        if (!existeUsuario(usuario)) {
+            int resultadoRegistro = usuarioDAO.modificarUsuario(usuario);
+            if (resultadoRegistro == 0) {
+                return "Registro Exitoso!";
+            } else {
+                return "Hubo un error en el registro, porfavor verifique los datos";
+            }
+        } else {
+            return "Usuario ya existente";
+        }
+    }
+
+    public Usuario buscarUsuarioPorNombreUsuario(String nombreUsuario) {
+        return usuarioDAO.buscarUsuarioPorNombreUsuario(nombreUsuario);
+    }
+
+    public Usuario buscarUsuarioPorId(int idUsuario) {
+            return usuarioDAO.buscarUsuarioPorId(idUsuario);
+    }
+
+    public boolean existeUsuario(Usuario usuario) {
+        if (usuarioDAO.buscarUsuarioPorId(usuario.getId()) == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
